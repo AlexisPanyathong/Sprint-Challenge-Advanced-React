fa867b6248a219d69c4fb1eb864e9e730a2707e4
Alexis Panyathong <alexispanyathong@gmail.com>
2019-09-06 12:17:17 -0500

    Updates
    I finished the sprint challenge and I am working on the dark mode styling. But I can't figure out how to change the navbar when switched to dark mode because I put the .dark-mode in the App.css and not in styled components.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useDarkMode } from './hooks/useDarkMode';
+import { NavHeader, Navbar } from './StyledWidgets';
 
 const NavBar = () => {
     const [darkMode, setDarkMode] = useDarkMode();
@@ -9,14 +10,18 @@ const NavBar = () => {
     };
 
     return (
-        <nav className="nav-bar">
-            <h1>Women's World Cup</h1>
-            <div className="dark-mode-toggle">
-                <div 
-                onClick={toggleMode} 
-                className={darkMode ? 'toggle toggled' : 'toggle'} />
-            </div>
-        </nav>
+        <Navbar>
+            <nav className="nav-bar">
+                <NavHeader>
+                    <h1>Women's World Cup</h1>
+                </NavHeader>
+                <div className="dark-mode-toggle">
+                    <div 
+                    onClick={toggleMode} 
+                    className={darkMode ? 'toggle toggled' : 'toggle'} />
+                </div>
+            </nav>
+        </Navbar>
     );
 };
 
--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { Card, Name, Country, Searches } from '../components/StyledWidgets';
 
 export default class PlayerCard extends React.Component {
 
     render() {
         return (
-            <div className="player-card">
-                {this.props.player.name}
-                {this.props.player.country}
-                {this.props.player.searches}
-            </div>
+
+            <Card>
+                <div className="player-card">
+                    <Name>
+                        {this.props.player.name}
+                    </Name>
+
+                    <Country>
+                        {this.props.player.country}
+                    </Country>
+
+                    <Searches>
+                        Searches: {this.props.player.searches}
+                    </Searches>
+                </div>
+            </Card>
         )
     }
 }
