f0f6971c3c946b7a3a35b372a29c3af127378543
Alexis Panyathong <alexispanyathong@gmail.com>
2019-09-06 14:36:26 -0500

    Updates on testing pt 2
    I installed the @testing-library/react and /jest-dom into the client directory and now all the testing passed.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as rtl from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
